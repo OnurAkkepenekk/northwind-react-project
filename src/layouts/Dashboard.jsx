4f84a6b24c857d318953e4f69df6d3c5d92213d7
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-07-03 14:49:19 +0300

    Create DashBoard component

---
+++ src/layouts/Dashboard.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Navi from "./Navi";
+import Categories from "./Categories";
+import ProductList from "../pages/ProductList";
+import { Grid } from "semantic-ui-react";
+export default function Dashboard() {
+  return (
+    <div>
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={4}>
+            <Categories></Categories>
+          </Grid.Column>
+          <Grid.Column width={12}>
+            <ProductList></ProductList>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </div>
+  );
+}
