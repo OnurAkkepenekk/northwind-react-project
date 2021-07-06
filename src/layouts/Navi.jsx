fd5450675341f78a557cb97fcfebbad54ff25a26
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-07-06 20:06:25 +0300

    Add router to project

--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -4,11 +4,13 @@ import CartSummary from "./CartSummary";
 import SignedOut from "./SignedOut";
 import SignIn from "./SignIn";
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 export default function Navi() {
   const [isAuthenticated, setIsAuthenticated] = useState(true);
-
+  let history = useHistory();
   function handleSignOut() {
     setIsAuthenticated(false);
+    history.push("/");
   }
   function handleSignIn() {
     setIsAuthenticated(true);
@@ -21,7 +23,11 @@ export default function Navi() {
           <Menu.Item name="Message" />
           <Menu.Menu position="right">
             <CartSummary />
-            {isAuthenticated ? <SignIn signOut={handleSignOut} /> : <SignedOut signIn={handleSignIn} />}
+            {isAuthenticated ? (
+              <SignIn signOut={handleSignOut} />
+            ) : (
+              <SignedOut signIn={handleSignIn} />
+            )}
           </Menu.Menu>
         </Container>
       </Menu>
