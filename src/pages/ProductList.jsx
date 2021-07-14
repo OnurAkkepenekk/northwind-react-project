7379a4a4aadf6aaea48a7416447e5e1790784f3a
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-07-14 19:10:27 +0300

    Update productList to state management

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,16 +1,27 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import { Icon, Label, Menu, Table } from "semantic-ui-react";
+import { Button, Icon, Label, Menu, Table } from "semantic-ui-react";
 import ProductService from "../services/productService";
+import { addToCart } from "../store/actions/cartActions";
+import { toast } from "react-toastify";
 export default function ProductList() {
-  
   const [products, setProducts] = useState([]);
-  useEffect(()=>{
-    let productService= new ProductService();
-    productService.getProducts().then(result => setProducts(result.data.data))
-  },[])
 
-  
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    let productService = new ProductService();
+    productService
+      .getProducts()
+      .then((result) => setProducts(result.data.data));
+  }, []);
+
+  const handleAddToCart = (product) => {
+    dispatch(addToCart(product));
+    toast.success(`${product.productName} sepete eklendi!`)
+  };
+
   return (
     <div>
       <Table celled>
@@ -21,17 +32,25 @@ export default function ProductList() {
             <Table.HeaderCell>Stok Adedi</Table.HeaderCell>
             <Table.HeaderCell>Açıklama</Table.HeaderCell>
             <Table.HeaderCell>Kategory</Table.HeaderCell>
+            <Table.HeaderCell></Table.HeaderCell>
           </Table.Row>
         </Table.Header>
 
         <Table.Body>
           {products.map((product) => (
             <Table.Row key={product.id}>
-              <Table.Cell><Link to={`/products/${product.productName}`}>{product.productName}</Link></Table.Cell>
+              <Table.Cell>
+                <Link to={`/products/${product.productName}`}>
+                  {product.productName}
+                </Link>
+              </Table.Cell>
               <Table.Cell>{product.unitPrice}</Table.Cell>
               <Table.Cell>{product.unitsInStock}</Table.Cell>
               <Table.Cell>{product.quantityPerUnit}</Table.Cell>
               <Table.Cell>{product.category.categoryName}</Table.Cell>
+              <Table.Cell>
+                <Button onClick={() => handleAddToCart(product)}>Sepete ekle</Button>
+              </Table.Cell>
             </Table.Row>
           ))}
         </Table.Body>
